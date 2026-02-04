3ffefefd324ad9a5b61045c229d03bbfe8ca1a16
Guilherme Casagrande <thegui4000@gmail.com>
2026-02-04 12:12:35 -0300

    fix<cors>: adding cors

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import transationRoutes from "./modules/transation/routes/transation.route";
+import cors from "cors";
 
 export const app = express();
 
 app.use(express.json());
+app.use(cors())
 
 app.get("/", (req, res) => {
     res.json({ status: "ok" });
--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,7 +1,7 @@
 import { app } from "./app";
 import { bootstrap } from "./bootstrap";
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 async function main() {
     await bootstrap();
