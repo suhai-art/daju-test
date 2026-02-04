3c38040667c63058df8d5edb3f06c6680ad2ee30
Guilherme Casagrande <thegui4000@gmail.com>
2026-02-04 11:14:58 -0300

    fix<bootstrap>: excluido banco de dados antes de iniciar app

--- back-end/src/bootstrap.ts
+++ back-end/src/bootstrap.ts
@@ -3,8 +3,17 @@ import { AppDataSource } from "./database/data-source";
 import { dataSeed } from "./database/data-seed";
 import TransationService from "./modules/transation/services/transation.service";
 import { TransactionEntity } from "./modules/transation/entity/transition.entity";
+import { access, unlink } from "fs/promises";
+import path from "path";
 
 export async function bootstrap(): Promise<void> {
+    try {
+        await access(path.join(__dirname, "./database/data/database.sqlite"));
+        await unlink(path.join(__dirname, "./database/data/database.sqlite"));
+    } catch {
+        console.log("Arquivo do banco não existe");
+    }
+
     await AppDataSource.initialize();
 
     const transactionRepository = AppDataSource.getRepository(TransactionEntity)
